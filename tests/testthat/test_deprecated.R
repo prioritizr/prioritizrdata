7807203b677d4e5ed802aaaa5a02fc05d0b78486
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2026-06-03 11:06:22 +1200

    fix tests

--- tests/testthat/test_deprecated.R
+++ tests/testthat/test_deprecated.R
@@ -1,9 +1,6 @@
 test_that("get_wa_features", {
   wa_species <- get_wa_species()
-  expect_warning(
-    wa_features <- get_wa_features(),
-    "deprecated"
-  )
+  wa_features <- suppressWarnings(get_wa_features())
   expect_is(wa_features, "SpatRaster")
   expect_equal(terra::values(wa_species), terra::values(wa_features))
 })
